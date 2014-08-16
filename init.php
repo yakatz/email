36bc5b86f2285dab9ad8556f6f93bd28499b7d16
Woody Gilk <woody.gilk@gmail.com>
2014-08-15 23:29:11 -0500

    use spaces for tabs

--- init.php
+++ init.php
@@ -3,6 +3,6 @@
 $charset = Kohana::$charset;
 
 Swift::init(function () use ($charset) {
-	// Set the default character set for everything
-	Swift_Preferences::getInstance()->setCharset($charset);
+    // Set the default character set for everything
+    Swift_Preferences::getInstance()->setCharset($charset);
 });
