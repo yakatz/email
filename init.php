e9c018395aec5469741ba885c912eb54559f2ea0
Woody Gilk <woody.gilk@gmail.com>
2014-08-19 19:06:18 -0500

    autoloader inception firewall

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php
 
-$charset = Kohana::$charset;
+Swift::init(function () {
+    $charset = Kohana::$charset;
 
-Swift::init(function () use ($charset) {
     // Set the default character set for everything
     Swift_Preferences::getInstance()->setCharset($charset);
 });
